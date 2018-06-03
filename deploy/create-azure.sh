85da45ecff919bae99be4264e880baac7fb34c8a
Andreas Willich <andreas.willich@gmail.com>
2018-06-03 20:36:50 +0200

    renaming azure function

--- deploy/create-azure.sh
+++ deploy/create-azure.sh
@@ -1,15 +1,24 @@
-#/bin/bash
+#/bin/bash -x
+
+set -x
 
 export resourceGroupName=irlbr
-export storageAccountName="$resourceGroupName_storageAccount"
+export storageAccountKey=storageaccount
+export storageAccountName=$resourceGroupName$storageAccountKey
 export region=northeurope
 
-az login
+
+az account show 1> /dev/null
+
+if [ $? != 0 ];
+then
+	az login
+fi
+
 az account set --subscription "Visual Studio Premium with MSDN"
 az group create --name $resourceGroupName --location $region
 
 az storage account create --name $storageAccountName --location $region --resource-group $resourceGroupName --sku Standard_LRS
 
 
-az functionapp create --resource-group $resourceGroupName --consumption-plan-location $region \
---name <app_name> --storage-account  $storageAccountName  
+az functionapp create --resource-group $resourceGroupName --consumption-plan-location $region --name irlbr --storage-account  $storageAccountName  
--- src/AzureFunction/Authenticate/Authentificate.fs
+++ src/AzureFunction/Authenticate/Authentificate.fs
@@ -1,11 +1,11 @@
-namespace OAuthAuthentification
+namespace IRLBR
 
 open Microsoft.AspNetCore.Mvc
 open Microsoft.AspNetCore.Http
 open Microsoft.Azure.WebJobs.Host
 
 
-module PaketHttp =
+module Authentificate =
   let run(req: HttpRequest, log: TraceWriter) =
     log.Info("F# HTTP trigger function processed a request.")
 
