3aaadd4285a454af2e5cd25cd34c88590cc0cc63
Andreas Willich <andreas.willich@gmail.com>
2018-06-03 16:42:22 +0200

    xamarin.auth working

--- src/IRLBR/App.xaml.fs
+++ src/IRLBR/App.xaml.fs
@@ -1,6 +1,30 @@
 ﻿namespace IRLBR
 
+open System
 open Xamarin.Forms
+open Xamarin.Auth
+
+
+type Constants =
+    static member Scope = "profile"
+    static member AuthorizeUrl = "https://accounts.google.com/o/oauth2/auth"
+    static member AccessTokenUrl = "https://www.googleapis.com/oauth2/v4/token"
+    static member clientId = "1053012469782-cq3lpcp9051ne51eoj2pslfeaqrioe8p.apps.googleusercontent.com"
 
 type App() =
     inherit Application(MainPage = MainPage())
+
+
+
+    override this.OnStart() =
+        let authenticator = new OAuth2Authenticator(
+                                Constants.clientId,
+                                null,
+                                Constants.Scope,
+                                new Uri(Constants.AuthorizeUrl),
+                                new Uri(redirectUri),
+                                new Uri(Constants.AccessTokenUrl),
+                                null,
+                                true)
+        ignore()
+                              
